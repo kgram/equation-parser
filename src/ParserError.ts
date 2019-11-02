fddce9eb098eef544323a00d1bbbf8f8fb73591c
Kristoffer Gram <kristoffer@subhub.dk>
2019-11-02 17:58:38 +0100

    Fix babel TS problem

--- src/ParserError.ts
+++ src/ParserError.ts
@@ -1,9 +1,13 @@
 import { EquationNodeParserError } from './EquationNode'
 
 export class ParserError extends Error {
+    position: number
+    type: EquationNodeParserError['errorType']
     values: any[]
-    constructor(public position: number, public type: EquationNodeParserError['errorType'], ...values: any[]) {
+    constructor(position: number, type: EquationNodeParserError['errorType'], ...values: any[]) {
         super(`Internal ${type} parse error`)
+        this.type = type
+        this.position = position
         this.values = values
     }
 }
