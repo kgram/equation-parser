1baaf8d4f2e5b7566df22e28d0353e3133b6992d
Kristoffer Gram <kristoffer@subaio.com>
2020-07-14 16:26:01 +0200

    Fix barrel exports of types

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,32 @@
-import * as EquationNode from './EquationNode'
-import * as EquationParserError from './EquationParserError'
-export type EquationNode = EquationNode.EquationNode
-export type EquationNodeVariable = EquationNode.EquationNodeVariable
-export type EquationNodeNumber = EquationNode.EquationNodeNumber
-export type EquationNodeFunction = EquationNode.EquationNodeFunction
-export type EquationNodeBlock = EquationNode.EquationNodeBlock
-export type EquationNodePositive = EquationNode.EquationNodePositive
-export type EquationNodeNegative = EquationNode.EquationNodeNegative
-export type EquationNodePositiveNegative = EquationNode.EquationNodePositiveNegative
-export type EquationNodePlus = EquationNode.EquationNodePlus
-export type EquationNodeMinus = EquationNode.EquationNodeMinus
-export type EquationNodePlusMinus = EquationNode.EquationNodePlusMinus
-export type EquationNodeMultiplyImplicit = EquationNode.EquationNodeMultiplyImplicit
-export type EquationNodeMultiplyDot = EquationNode.EquationNodeMultiplyDot
-export type EquationNodeMultiplyCross = EquationNode.EquationNodeMultiplyCross
-export type EquationNodeDivideFraction = EquationNode.EquationNodeDivideFraction
-export type EquationNodeDivideInline = EquationNode.EquationNodeDivideInline
-export type EquationNodePower = EquationNode.EquationNodePower
-export type EquationNodeEquals = EquationNode.EquationNodeEquals
-export type EquationNodeLessThan = EquationNode.EquationNodeLessThan
-export type EquationNodeGreaterThan = EquationNode.EquationNodeGreaterThan
-export type EquationNodeLessThanEquals = EquationNode.EquationNodeLessThanEquals
-export type EquationNodeGreaterThanEquals = EquationNode.EquationNodeGreaterThanEquals
-export type EquationNodeApproximates = EquationNode.EquationNodeApproximates
-export type EquationNodeMatrix = EquationNode.EquationNodeMatrix
-export type EquationParserError = EquationParserError.EquationParserError
+export type {
+    EquationNode,
+    EquationNodeVariable,
+    EquationNodeNumber,
+    EquationNodeFunction,
+    EquationNodeBlock,
+    EquationNodePositive,
+    EquationNodeNegative,
+    EquationNodePositiveNegative,
+    EquationNodePlus,
+    EquationNodeMinus,
+    EquationNodePlusMinus,
+    EquationNodeMultiplyImplicit,
+    EquationNodeMultiplyDot,
+    EquationNodeMultiplyCross,
+    EquationNodeDivideFraction,
+    EquationNodeDivideInline,
+    EquationNodePower,
+    EquationNodeEquals,
+    EquationNodeLessThan,
+    EquationNodeGreaterThan,
+    EquationNodeLessThanEquals,
+    EquationNodeGreaterThanEquals,
+    EquationNodeApproximates,
+    EquationNodeMatrix,
+} from './EquationNode'
+export type {
+    EquationParserError,
+} from './EquationParserError'
 
 export { parse } from './parse'
 export { renderTree } from './renderTree'
